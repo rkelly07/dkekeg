60318b8e09ef979b09185d373fafba163f191823
Ryan Kelly <rhkelly@mit.edu>
2016-10-24 14:48:53 -0400

    done for good

--- DBAccessor.py
+++ DBAccessor.py
@@ -59,7 +59,6 @@ class DBAccessor:
 	def getKerberos(self, name):
 		t = (name,)
 		name_statement = "SELECT id FROM BROTHERS WHERE name=?"
-		print name
 		try:
 			self.c.execute(name_statement,t)
 			kerberos = self.c.fetchone()[0]
